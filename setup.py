0873a67e2dd4a9b8cbdb5764fa906034a7b2fc5d
Cedric Duriau <duriau.cedric@live.be>
2019-04-27 23:19:28 +0200

    refactor description

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(name="watermarkbuddy",
       version="0.1.0",
-      description="Watermarking tool using ffprobe and ffmpeg.",
+      description="Watermarking tool using ffmpeg.",
       license="MIT",
       author="C&eacute;dric Duriau",
       author_email="duriau.cedric@live.be",
