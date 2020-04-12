5295ed21b0880f1d361c027d9da47ef21864fd0d
Cedric Duriau <duriau.cedric@live.be>
2020-04-12 13:20:25 +0200

    use requires

--- setup.py
+++ setup.py
@@ -1,9 +1,21 @@
 # stdlib modules
-from setuptools import setup, find_packages
+import os
+import sys
+from setuptools import setup
+from setuptools import find_packages
+
+# tool modules
+f = os.path.abspath(__file__)
+package_dir = os.path.join(os.path.dirname(f), "python")
+sys.path.insert(0, package_dir)
+from watermarkbuddy import __version__  # noqa
+
+requirements_dev = ["flake8", "radon"]
+requirements_install = ["PySide2"]
 
 
 setup(name="watermarkbuddy",
-      version="1.0.0",
+      version=__version__,
       description="Watermarking tool using ffmpeg.",
       license="GPLv3",
       author="C&eacute;dric Duriau",
@@ -11,4 +23,6 @@ setup(name="watermarkbuddy",
       url="https://github.com/cedricduriau/watermarkbuddy",
       packages=find_packages(where="python"),
       package_dir={"": "python"},
-      scripts=["bin/watermarkbuddy", "bin/watermarkbuddycmd"])
+      scripts=["bin/watermarkbuddy", "bin/watermarkbuddycmd"],
+      install_requires=requirements_install,
+      extras_require={"dev": requirements_dev})
